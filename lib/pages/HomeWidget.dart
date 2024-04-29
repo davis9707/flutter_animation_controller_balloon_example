de3f100ca57dc327865621535a4ba64a6dcc454a
firdavs9777 <fmutalipov97@gmail.com>
2024-04-29 15:28:26 +0900

    Initial Comment

---
+++ lib/pages/HomeWidget.dart
@@ -0,0 +1,18 @@
+import 'package:ch7_animation_controller/pages/animated_balllon.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnimatedBalloon(),
+    );
+  }
+}
